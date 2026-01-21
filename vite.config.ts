1df9b9398305e8075e0270013846d5eefc02dffb
AlexseyPolatov <alekseypolatow@gmail.com>
2026-01-21 16:43:51 +0300

    create

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/weather': {
+        target: 'https://wttr.in',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/weather/, ''),
+      },
+    },
+  },
+})
